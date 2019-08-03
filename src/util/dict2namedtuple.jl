f196ca34f7741fda6787284a25ce50a114b9243d
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2019-08-03 15:22:04 -0400

    On branch master new file:   dict2namedtuple.jl

---
+++ src/util/dict2namedtuple.jl
@@ -0,0 +1,34 @@
+export dict2namedtuple
+
+"""
+`function nt = dict2namedtuple(D)`
+
+Convert Dict (key, value pairs) to NamedTuple
+
+Example:
+
+D = Dict("A"=>1, "B"=>2)
+
+nt = dict2namedtuple(D)
+
+nt.A == D["A"]     # true
+nt.B == D["B"]     # true
+
+""" 
+function dict2namedtuple(D)
+
+	# key keys and convert to Symbol array
+	k = collect(keys(D))
+
+	sym = Array{Symbol}(undef, length(k))
+	for ii = 1:length(k)
+		sym[ii] = Symbol(k[ii])
+	end
+	
+	# get values
+	v = collect(values(D))
+
+	# convert to named tuple
+	return NamedTuple{Tuple(sym)}(v)
+
+end
