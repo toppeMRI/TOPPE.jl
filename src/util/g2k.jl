c131199f305d95153e276aa5e9f84cec16352acf
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2019-08-03 11:09:21 -0400

    On branch master modified:   g2k.jl

--- src/util/g2k.jl
+++ src/util/g2k.jl
@@ -1,12 +1,16 @@
 using Interpolations
 
 """
-`(kx,ky) = g2k(g,nint=1,delay=(0,0))`
+`(kx,ky) = g2k(g, nint=1, delay=(0,0))`
 
 in
-- g            [N 2] array containing Gx and Gy in Gauss/cm
+- g            [N 2] array of Real numbers containing Gx and Gy in Gauss/cm
 - nint         number of leaf rotations
 - delay        Tuple{<:Real,<:Real} containing shift in gx and gy, in units of samples
+
+out
+- kx           [N nint] array containing kx locations (cycles/cm; Real)
+- ky           [N nint] array containing ky locations (cycles/cm; Real)
 """
 function g2k(
 	g::Array{<:Real,2},
@@ -33,11 +37,15 @@ function g2k(
 	k = gamma*dt*cumsum(g, dims=1)     # cycles/cm
 
 	# apply (in-plane) rotations
-	kc = zeros(Complex{Float64},n,nint);
+	kc = Array{Complex{Float64}}(undef,n,nint);
 	kprot = k[:,1] + 1im*k[:,2]
 	for ii = 1:nint
 		phi = (ii-1)/nint*2pi
 		kc[:,ii] = kprot.*exp.(1im*phi)
+	end
+
+	if nint==1
+		kc = kc[:,1]     # return vector
 	end
 
 	return (real(kc), imag(kc))
