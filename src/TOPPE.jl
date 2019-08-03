ddfc5ddfd1a97384c8f7cd642568c2a04f2bc96c
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2019-08-03 13:16:43 -0400

    modified:   TOPPE.jl

--- src/TOPPE.jl
+++ src/TOPPE.jl
@@ -2,6 +2,12 @@
 
 """
 `TOPPE` is the Julia version of the TOPPE Matlab toolbox for MR pulse programming, https://toppemri.github.io/
+
+Usage:
+
+julia> include("TOPPE.jl")
+
+julia> using Main.TOPPE
 """
 module TOPPE
    # using Reexport
